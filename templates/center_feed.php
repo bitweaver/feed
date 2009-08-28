8e76a5475bb24be294c4e9798844f8a6956e9182
Christian Fowler <spider@viovio.com>
2009-08-28 20:09:02 +0000

    add some center modules

---
+++ templates/center_feed.php
@@ -0,0 +1,19 @@
+<?php
+
+global $gBitDb,$gQueryUser,$gBitSmarty;
+
+require_once( FEED_PKG_PATH.'feed_lib.php' );
+
+if( !empty($gQueryUser) ){
+
+	$listHash['user_id'] = $gQueryUser->mUserId;
+	$actions = feed_get_actions( $listHash );
+	$gBitSmarty->assign( 'actions', $actions);
+
+
+}
+
+
+
+
+?>
---
+++ templates/center_feed.tpl
@@ -0,0 +1,7 @@
+<div style="margin-top:10px;">
+	{foreach from=$actions item='action'}
+		<div>
+			{*{$action.icon}*} {$action.real_log} on {$action.last_modified|date_format}
+		</div>
+	{/foreach}
+</div>
