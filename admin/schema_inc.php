5563a0d3e292b8f9f259a2d61155315d98d6f189
Christian Fowler <spider@viovio.com>
2009-08-31 16:17:36 +0000

    add icon for feed_icon_url

--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -3,6 +3,7 @@ $tables = array(
   'feed_conjugation' => "
 	content_type_guid C(16) NOTNULL,
 	conjugation_phrase C(255) NOTNULL,
+	feed_icon_url C(255),
 	is_target_linked   C(1)
     CONSTRAINT ', CONSTRAINT `feed_conj_content_type_ref` FOREIGN KEY (`content_type_guid`) REFERENCES `".BIT_DB_PREFIX."liberty_content_types` ( `content_type_guid` )'
   "
@@ -15,8 +16,8 @@ foreach( array_keys( $tables ) AS $tableName ) {
 }
 
 $gBitInstaller->registerSchemaDefault( FEED_PKG_NAME, array(
-
-	"INSERT INTO `".BIT_DB_PREFIX."feed_conjugation` (`content_type_guid`, `conjugation_phrase`, `is_target_linked`) VALUES ('bituser', 'updated their profile', 'y')",
+	"INSERT INTO `".BIT_DB_PREFIX."feed_conjugation` (`content_type_guid`, `conjugation_phrase`, `is_target_linked`,`feed_icon_url`) VALUES ('bituser', 'updated their profile', 'y','".FEED_PKG_URL."icons/pixelmixerbasic/user_16.png')",
+	"INSERT INTO `".BIT_DB_PREFIX."feed_conjugation` (`content_type_guid`,`conjugation_phrase`,`is_target_linked`,`feed_icon_url`) VALUES ('bitcomment','commented','y','".FEED_PKG_URL."icons/pixelmixerbasic/bubble_16.png')",
 ));
 
 $gBitInstaller->registerPackageInfo( FEED_PKG_NAME, array(
@@ -29,4 +30,5 @@ $gBitInstaller->registerUserPermissions( FEED_PKG_NAME, array(
 	array( 'p_feed_admin', 'Can admin feeds', 'admin', FEED_PKG_NAME ),
 	array( 'p_feed_view', 'Can view feed', 'basic', FEED_PKG_NAME ),
 ) );
+
 ?>
