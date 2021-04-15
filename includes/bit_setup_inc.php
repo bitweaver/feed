10cb47612c274a0b7d2f69daa2e2be3e2b3ca445
modela bitweaver <spiderr@bitweaver.org>
2021-04-15 12:15:02 -0400

    clean up for includes/ reorg

--- includes/bit_setup_inc.php
+++ includes/bit_setup_inc.php
@@ -3,7 +3,7 @@ global $gBitSystem, $gBitUser, $gBitThemes;
 
 $registerHash = array(
 	'package_name' => 'feed',
-	'package_path' => dirname( __FILE__ ).'/',
+	'package_path' => dirname( dirname( __FILE__ ) ).'/',
 );
 
 $gBitSystem->registerPackage( $registerHash );
